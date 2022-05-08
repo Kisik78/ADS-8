81dce73bf2da86c8912f730ff89c2cc10db86a41
Коньков Арсений <95272498+Kisik78@users.noreply.github.com>
2022-05-08 19:19:53 +0300

    Konkov

--- include/train.h
+++ include/train.h
@@ -10,8 +10,8 @@ class Train {
     };
     int countOp; // счетчик шагов (число переходов из вагона в вагон)
     Cage* first; // точка входа в поезд (первый вагон)
-    Cage* tail, * current;
-    Train::Cage* create(bool light) {
+    Cage* tail, * uni;
+    Train::Cage* createv(bool light) {
         Cage* cage = new Cage;
         cage->light = light;
         cage->next = nullptr;
@@ -23,7 +23,7 @@ class Train {
     }
 
  public:
-    Train(): first(nullptr), tail(nullptr), current(nullptr), countOp(0) {}
+    Train(): first(nullptr), tail(nullptr), uni(nullptr), countOp(0) {}
     void addCage(bool light); // добавить вагон с начальным состоянием лампочки
     int getLength();          // вычислить длину поезда
     int getOpCount();  // вернуть число переходов (из вагона в вагон)
